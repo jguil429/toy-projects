fb345822bd49de0a052aea6fe435a1b8002e168b
Jennie <jguil429@gmail.com>
2024-07-08 19:08:42 -0500

    rescon for real this time

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,28 +1,11 @@
 import styles from './Button.module.css';
-<<<<<<< HEAD
-import React from 'react';
-
-
-function Button({ onClick, buttonText }) {
-
-    return (
-        <button onClick={ onClick }
-                className={ styles.button }>
-                { buttonText }
-=======
 
 function Button({ onClick, buttonText }) {
     return (
         <button className={styles.button} onClick={onClick}>
             {buttonText}
->>>>>>> 60dd9bbd688cccd4be56503cbacb3fb9020111e3
         </button>
     );
 }
 
-<<<<<<< HEAD
-
-export default Button
-=======
 export default Button;
->>>>>>> 60dd9bbd688cccd4be56503cbacb3fb9020111e3
