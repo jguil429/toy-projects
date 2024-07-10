4da01f287b95f1dd3abf2fd63b33fb876e639c78
Jordan Steel <jordansteel@randallreilly.com>
2024-07-09 23:35:02 -0500

    fix homepage spacing

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -6,21 +6,24 @@ import placeHolderImg from '../assets/placeholderimg.jpg';
 import Button from '../Button/Button';
 
 function Card({ toyTitle, toyDescription, route }) {
-  return (
-    <div className={styles.card}>
-      <img className={styles.cardimg} src={placeHolderImg} alt="placeholder image" />
-      <h2 className={styles.cardtitle}>{toyTitle}</h2>
-      <p className={styles.cardtext}>{toyDescription}</p>
-      <Link to={route}>
-        <Button className={styles.cardButton} buttonText='Play with toy' />
-      </Link>
-    </div>
-  );
+// Card Component
+    return (
+        <div className={styles.card}>
+            <div className={styles.cardContent}>
+                <img className={styles.cardimg} src={placeHolderImg} alt="placeholder image" />
+                <h2 className={styles.cardtitle}>{toyTitle}</h2>
+                <p className={styles.cardtext}>{toyDescription}</p>
+            </div>
+            <Link to={route} className={styles.cardLink}>
+                <Button className={styles.cardButton} buttonText='Play with toy' />
+            </Link>
+        </div>
+    );
 }
 
 Card.propTypes = {
-  toyTitle: PropTypes.string.isRequired,
-  toyDescription: PropTypes.string.isRequired,
+    toyTitle: PropTypes.string.isRequired,
+    toyDescription: PropTypes.string.isRequired,
   route: PropTypes.string.isRequired,
 };
 
